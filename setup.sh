c4a68c175fd63e15139ea8919f5be0cdd67e0163
neoan <stefan.roehrl@gmail.com>
2021-04-15 12:40:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-
+#! /usr/bin/env bash
 
     # Install apache server , mysql
-    echo \"** 1/6 Install Apache & MySQL ** \"
+    echo "** 1/6 Install Apache & MySQL ** "
     apt-get -qq install software-properties-common -y
     add-apt-repository ppa:ondrej/php -y
     apt-get update
@@ -9,30 +9,30 @@
     apt-get -qq install -y mysql-server
 
     # Create default database
-    echo \"** 2/6 Create & Setup default database neoan3 ** \"
-    mysql -e \"create database neoan3\ \"
-    mysql -e \"create user 'neoan3'@'localhost'\ \"
-    mysql -e \"grant all privileges on neoan3.* to 'neoan3'@'localhost'\ \"
-    mysql -e \"flush privileges \"
+    echo "** 2/6 Create & Setup default database neoan3 ** "
+    mysql -e "create database neoan3\ "
+    mysql -e "create user 'neoan3'@'localhost'\ "
+    mysql -e "grant all privileges on neoan3.* to 'neoan3'@'localhost'\ "
+    mysql -e "flush privileges "
 
     # Install PHP8 & modules
-    echo \"** 3/6 Install PHP8 & modules ** \"
+    echo "** 3/6 Install PHP8 & modules ** "
     apt-get -qq install -y php8.0 libapache2-mod-php8.0 php8.0-{mysql,zip,xml,curl,mbstring} curl git
     apt-get upgrade
 
     # Install & setup Composer
-    echo \"** 4/6 Install & Setup composer** \"
+    echo "** 4/6 Install & Setup composer** "
     curl -sS https://getcomposer.org/installer -o composer-setup.php
     php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
     # Install & setup neoan3 cli
-    echo \"** 5/6 Install & Setup neoan3 cli** \"
+    echo "** 5/6 Install & Setup neoan3 cli** "
     sudo -u vagrant -i composer global require neoan3/neoan3
     mkdir /credentials
     echo '{\"testing_db\":{\"host\":\"localhost\",\"user\":\"neoan3\",\"name\":\"neoan3\",\"assumes_uuid\":true}}' > /credentials/credentials.json
     chown vagrant:vagrant -R /credentials
-    echo \"PATH=$PATH:/home/vagrant/.config/composer/vendor/bin\" >> /home/vagrant/.profile
-    echo \"cd /var/www/html\" >> /home/vagrant/.profile
+    echo "PATH=$PATH:/home/vagrant/.config/composer/vendor/bin" >> /home/vagrant/.profile
+    echo "cd /var/www/html" >> /home/vagrant/.profile
     NEOAN_APP=/var/www/html/version.json
     if [ ! -f \"$NEOAN_APP\" ]; then
         cd /var/www/html
@@ -42,8 +42,8 @@
     fi
 
     # Configure Apache
-    echo \"** 6/6 configure Apache** \"
-    echo \"\<VirtualHost *:80\>
+    echo "** 6/6 configure Apache** "
+    echo "\<VirtualHost *:80\>
         DocumentRoot /var/www/html
         AllowEncodedSlashes On
         \<Directory /var/www/html\>
@@ -58,9 +58,9 @@
         \</Directory\>
         ErrorLog ${APACHE_LOG_DIR}/error.log
         CustomLog ${APACHE_LOG_DIR}/access.log combined
-    \</VirtualHost\>\" > /etc/apache2/sites-available/000-default.conf
+    \</VirtualHost\>" > /etc/apache2/sites-available/000-default.conf
     a2enmod rewrite
     service apache2 restart
-    echo \"** neoan3 box running... visit http://192.168.33.10 in your browser for to view the application ** \"
-    echo \"** .. vagrant ssh .. ** \"
-    echo \"** ... synced working directory .. ** \"
+    echo "** neoan3 box running... visit http://192.168.33.10 in your browser for to view the application ** "
+    echo "** .. vagrant ssh .. ** "
+    echo "** ... synced working directory .. ** "
